ce6ab04a94b0d0e4fb814681200365226d20831e
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-10-05 14:35:48 +0300

    add bitcast, powerof2, and more implementation\ ref del whitespace

--- 2DPhysicEngine/Physics/Math.h
+++ 2DPhysicEngine/Physics/Math.h
@@ -7,21 +7,21 @@
 
 namespace Math
 {
-const float Pi = 3.1415926535f;
-const float TwoPi = Pi * 2.0f;
-const float PiOver2 = Pi / 2.0f;
-constexpr float Infinity = std::numeric_limits<float>::infinity();
-constexpr float NegInfinity = -std::numeric_limits<float>::infinity();
+static constexpr float Pi = 3.1415926535f;
+static constexpr float TwoPi = Pi * 2.0f;
+static constexpr float PiOver2 = Pi / 2.0f;
+static constexpr float Infinity = std::numeric_limits<float>::infinity();
+static constexpr float NegInfinity = -std::numeric_limits<float>::infinity();
 
 
 inline float ToRadians(float degrees)
 {
-    return degrees * Pi / 180.0f;
+    return degrees * ( Pi / 180.0f );
 }
 
 inline float ToDegrees(float radians)
 { 
-    return radians * 180.0f / Pi;
+    return radians * ( 180.0f / Pi );
 }
 
 inline bool NearZero(float val, float epsilon = 0.001f)
@@ -103,6 +103,51 @@ constexpr const T& Clamp(const T& val, const T& lower, const T& upper)
 {
     return Min(upper, Max(lower, val));
 }
+
+
+/**
+ * Check if inV is a power of
+ */
+template <typename T>
+constexpr bool IsPowerOfTwo(T inV)
+{
+    return (inV & (inV - 1)) == 0;
+}
+
+/**
+ * Get the sign of a value 
+ */
+template <typename T> 
+constexpr T Sign(T inV)
+{
+    return inV < 0 ? T(-1) : T(1)
+}
+
+/**
+ * Simple implementation power of 2 of a value, or the itself if the value is already a power of 2
+ */
+template <class  To, class From>
+To BitCast(const From& InValue)
+{
+    static_assert(std::is_trivially_constructible_v<To>);
+    static_assert(sizeof(From) == sizeof(To));
+
+    union FromTo
+    {
+        To      mTo;
+        From    mFrom;
+    }
+
+    FromTo convert;
+    convert.mFrom = InValue;
+    return convert.mTo;
+}
+
+
+
+
+
+
 
 }
 
