c2c6fd2d692c3425d266e8e15861efaf9cf481b5
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-11-06 16:59:47 +0300

    update main.cpp file

--- 2DGameEngine/src/main.cpp
+++ 2DGameEngine/src/main.cpp
@@ -4,12 +4,16 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include <glm/glm.hpp> 
+#include <imgui/imgui.h>
+#include <sol/sol.hpp> 
 
 int main()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	glm::vec2 velocity = glm::vec2(3.0f, 3.0f);
-	
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+		
 	
 	
 	std::cout << "Hello world!!!" << std::endl;
