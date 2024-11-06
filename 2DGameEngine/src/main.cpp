a74051352e16beacd7e1dacac5793df24e828c6a
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-11-06 16:54:50 +0300

    add imgui sol glm library

--- 2DGameEngine/src/main.cpp
+++ 2DGameEngine/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp> 
 
 int main()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	glm::vec2 velocity = glm::vec2(3.0f, 3.0f);
+	
+	
+	
 	std::cout << "Hello world!!!" << std::endl;
 	return 0;
 }
