e9a07c79e41da440ea13dc37590bad1edd28c5f2
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-11-07 22:27:28 +0300

    add pool class and ipool interface

--- 2DGameEngine/src/ECS/ECS.h
+++ 2DGameEngine/src/ECS/ECS.h
@@ -30,9 +30,10 @@ private:
 
 public:
     Entity(int id) : ID(id){}
-
+    Entity(const Entity& other) = default;
     int GetID() const;
 
+    Entity& operator = (const Entity& other) = default;
     bool operator==(const Entity& other) const { return ID == other.ID; }
     bool operator!=(const Entity& other) const { return ID != other.ID; }
     bool operator<(const Entity& other) const { return ID < other.ID; }
@@ -59,13 +60,81 @@ public:
     void RequireComponent();
 };
 
-class Registry
+class IPool
 {
+public:
+    virtual ~IPool() {}
+
 };
 
+template<class T>
+class Pool : public IPool
+{
+private:
+    std::vector<T> data;
 
+public:
+    Pool(int size = 100)
+    {
+        data.resize(size);
+    }
 
-#endif
+    virtual ~Pool() = default;
+
+    bool IsEmpty() const
+    {
+        return data.empty();
+    }
+
+    int GetSize() const
+    {
+        return data.size();
+    }
+
+    void Resize(int size)
+    {
+        data.resize(size);
+    }
+
+    void Clear()
+    {
+        data.clear();
+    }
+
+    void Add(T object)
+    {
+        data.add(object);
+    }
+
+    void Set(int index, T object)
+    {
+        data[index] = object;
+    }
+
+    T& Get(int index) 
+    {
+        return static_cast<T&>(data[index]);
+    }
+
+    T& operator[](unsigned int index)
+    {
+        return data[index];
+    }
+};
+
+class Registry
+{
+private:
+    int numEntity = 0;
+
+public:
+    Registry() = default;
+
+    Entity CreateEntity();
+    void KillEntity(Entity entity);
+    void 
+};
+
 
 template <typename TComponent>
 inline void System::RequireComponent()
@@ -73,3 +142,8 @@ inline void System::RequireComponent()
     const auto componentID = Component<TComponent>::GetID();
     componentSignature.set(componentID);
 }
+
+
+#endif
+
+
