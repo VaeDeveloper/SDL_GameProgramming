4b93bca5be364d0a944511944c6f2364769b77ef
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-11-16 02:16:35 +0300

    add chain_app in repo

---
+++ 2DPhysicEngine/Chain/Application.h
@@ -0,0 +1,46 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "Graphics.h"
+#include <vector>
+#include "../Physics/World.h"
+#include "../Physics/Particle.h"
+
+class Body;
+
+/**
+* @Discrioption: Application Class (
+* @func     IsRunning();
+* @func     Setup();
+* @func     Input();
+* @func     Update();
+* @func     Render();
+* @func     Destroy();
+*/
+class Application
+{
+private:
+        bool running = false;
+        std::vector<Particle*> particles;
+        Vector2D pushForce = Vector2D(0, 0);
+        Vector2D mouseCursor = Vector2D(0, 0);
+        bool leftMouseButtonDown = false;
+
+        Vector2D anchor;
+        float k = 300;
+        float restLength = 15;
+        const int NUM_PARTICLES = 15;
+        
+public:
+    Application() = default;
+    ~Application() = default;
+    bool IsRunning() const;
+    void Setup();
+    void Input();
+    void Update();
+    void Render();
+    void RenderObjects();
+    void Destroy();
+    float TimeDeductions();
+};
+#endif
---
+++ 2DPhysicEngine/Chain/Graphics.cpp
@@ -0,0 +1,139 @@
+#include "Graphics.h"
+#include <iostream>
+
+SDL_Window* Graphics::window = nullptr;
+SDL_Renderer* Graphics::Renderer = nullptr;
+int Graphics::windowWidth = 10;
+int Graphics::windowHeight = 10;
+
+int Graphics::Width()
+{
+    return windowWidth;
+}
+
+int Graphics::Height()
+{
+    return windowHeight;
+}
+
+bool Graphics::OpenWindow()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "Error initializing SDL" << std::endl;
+        return false;
+    }
+
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+    windowWidth = display_mode.w;
+    windowHeight = display_mode.h;
+    window = SDL_CreateWindow(NULL, 0, 0, windowWidth, windowHeight, SDL_WINDOW_BORDERLESS);
+
+    if (!window)
+    {
+        std::cerr << "Error creating SDL window" << std::endl;
+        return false;
+    }
+
+    Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (!Renderer)
+    {
+        std::cerr << "Error creating SDL renderer" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void Graphics::ClearScreen(Uint32 color)
+{
+    SDL_SetRenderDrawColor(Renderer, color >> 16, color >> 8, color, 255);
+    SDL_RenderClear(Renderer);
+}
+
+void Graphics::RenderFrame()
+{
+    SDL_RenderPresent(Renderer);
+}
+
+void Graphics::DrawLine(int x0, int y0, int x1, int y1, Uint32 color)
+{
+    lineColor(Renderer, x0, y0, x1, y1, color);
+}
+
+void Graphics::DrawPoint(int x, int y, Uint32 color)
+{
+    SDL_SetRenderDrawColor(Renderer, color >> 16, color >> 8, color, 255);
+    SDL_RenderDrawPoint(Renderer, x, y);
+}
+
+void Graphics::DrawCircle(int x, int y, int radius, float angle, Uint32 color)
+{
+    circleColor(Renderer, x, y, radius, color);
+    lineColor(Renderer, x, y, x + cos(angle) * radius, y + sin(angle) * radius, color);
+}
+
+void Graphics::DrawFillCircle(int x, int y, int radius, Uint32 color)
+{
+    filledCircleColor(Renderer, x, y, radius, color);
+}
+
+void Graphics::DrawRect(int x, int y, int width, int height, Uint32 color)
+{
+    lineColor(Renderer, x - width / 2.0, y - height / 2.0, x + width / 2.0, y - height / 2.0, color);
+    lineColor(Renderer, x + width / 2.0, y - height / 2.0, x + width / 2.0, y + height / 2.0, color);
+    lineColor(Renderer, x + width / 2.0, y + height / 2.0, x - width / 2.0, y + height / 2.0, color);
+    lineColor(Renderer, x - width / 2.0, y + height / 2.0, x - width / 2.0, y - height / 2.0, color);
+}
+
+void Graphics::DrawFillRect(int x, int y, int width, int height, Uint32 color)
+{
+    boxColor(Renderer, x - width / 2.0, y - height / 2.0, x + width / 2.0, y + height / 2.0, color);
+}
+
+void Graphics::DrawPolygon(int x, int y, const std::vector<Vector2D>& vertices, Uint32 color)
+{
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        int currIndex = i;
+        int nextIndex = (i + 1) % vertices.size();
+        lineColor(Renderer, vertices[currIndex].x, vertices[currIndex].y, vertices[nextIndex].x, vertices[nextIndex].y, color);
+    }
+    filledCircleColor(Renderer, x, y, 1, color);
+}
+
+void Graphics::DrawFillPolygon(int x, int y, const std::vector<Vector2D>& vertices, Uint32 color)
+{
+    std::vector<short> vx;
+    std::vector<short> vy;
+
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        vx.push_back(static_cast<int>(vertices[i].x));
+    }
+
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        vy.push_back(static_cast<int>(vertices[i].y));
+    }
+
+    filledPolygonColor(Renderer, &vx[0], &vy[0], vertices.size(), color);
+    filledCircleColor(Renderer, x, y, 1, 0xFF000000);
+}
+
+void Graphics::DrawTexture(int x, int y, int width, int height, float rotation, SDL_Texture* texture)
+{
+    SDL_Rect dstRect = {x - (width / 2), y - (height / 2), width, height};
+    float rotationDeg = rotation * 57.2958;
+
+    SDL_RenderCopyEx(Renderer, texture, NULL, &dstRect, rotationDeg, NULL, SDL_FLIP_NONE);
+}
+
+void Graphics::CloseWindow(void)
+{
+    SDL_DestroyRenderer(Renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
